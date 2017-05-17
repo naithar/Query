0df52ca1836843b0b76e0e07b205a8d482018600
Sergey Minakov <devias.naith@gmail.com>
2017-05-17 19:04:53 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Query",
 	dependencies: [
-		.Package(url: "https://github.com/naithar/Wrap.git", majorVersion: 0, minor: 1)
+		.Package(url: "https://github.com/naithar/Wrap.git", majorVersion: 0, minor: 2)
 	]
 )
